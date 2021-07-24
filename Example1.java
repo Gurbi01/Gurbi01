3fadbf533b93b3020822f34a7d2a84e10eba111c
Gurbi01 <87616368+Gurbi01@users.noreply.github.com>
2021-07-24 13:03:30 -0400

    Java Basics
    if - else - else if - Demo

---
+++ Example1.java
@@ -0,0 +1,20 @@
+package weekend_classes.july.basics;
+
+/**
+ * 
+ * if - else - else if - Demo
+ * in the last example include second division if marks less than 60
+ * first division if marks between 60 and 75
+ * and distinction if marks more than 75
+ *
+ */
+public class Example1 {
+
+	public static void main(String[] args) {
+		int marks = 60;
+		
+		
+
+	}
+
+}
